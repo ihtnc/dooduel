4eb09886aa2d621de8d61e94a23d86b1b443d52f
ihtnc <ihopethisnamecounts@yahoo.com>
2025-12-08 15:34:35 +1100

    Fixed supabase realtime issue

--- db/publications/400-publication-real-time-messages.sql
+++
@@ -1,4 +0,0 @@
--- Create a publication for the outbox table to enable real-time messaging
--- Triggers on the app.outbox table will send messages to subscribed clients
-
-ALTER PUBLICATION supabase_realtime_messages_publication ADD TABLE app.outbox;
